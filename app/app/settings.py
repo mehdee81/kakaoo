b12ff8df6e56f92c522b353392c73f0e224a9ccc
Mori 30R <ghahreman00@gmail.com>
2024-03-15 17:10:00 +0330

    Add Live Reload To Django

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "livereload",
     "django.contrib.staticfiles",
     "scheduler",
 ]
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "livereload.middleware.LiveReloadScript",
 ]
 
 ROOT_URLCONF = "app.urls"
